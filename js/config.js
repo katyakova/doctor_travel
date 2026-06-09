11409fd87eea45b5e916d00480a4abc348352bcf
katyakova <133378450+katyakova@users.noreply.github.com>
2026-06-09 11:09:45 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// js/config.js
+const MD_CONFIG = {
+  credentials: {
+    username: "client",      // поменяй на нужный логин
+    password: "password"  // поменяй на нужный пароль
+  },
+  authTokenValue: "md_local_token_v1"
+};
